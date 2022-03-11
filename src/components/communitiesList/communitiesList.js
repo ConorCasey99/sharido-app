2eec2d5e76400e0397b395f9ba1697dcfb00c2cc
Conor Casey <conorcasey258@gmail.com>
2022-03-11 15:42:10 +0000

    Read from communities implemented

--- src/components/communitiesList/communitiesList.js
+++ src/components/communitiesList/communitiesList.js
@@ -1,4 +1,4 @@
-import React,  {useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import {
@@ -10,14 +10,14 @@ import {
   IonContent,
   IonAvatar,
   IonPage,
-  IonRouterLink
+  IonRouterLink,
 } from "@ionic/react";
 
 import firebase from "../../firebase";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { async } from "@firebase/util";
 import { db } from "../../firebase";
-import "./communitiesList.css"
+import "./communitiesList.css";
 import { updateCurrentUser } from "firebase/auth";
 import { useAuthentication } from "../../contexts/authentication/AuthenticationContext";
 import { link } from "fs";
@@ -28,26 +28,27 @@ const CommunitiesList = () => {
   const [communityList, setCommunitiesList] = useState([]);
   const communitiesCollectionRef = collection(db, "communities");
   const { currentUser } = useAuthentication();
-  
+
   useEffect(() => {
     const getCommunities = async () => {
       const communities = await getDocs(communitiesCollectionRef);
-      setCommunitiesList(communities.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      setCommunitiesList(
+        communities.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
     };
     getCommunities();
   });
 
   const deleteCommunity = async (id, admin) => {
-    if (admin !== currentUser.email){
-      deleteAlert()
-    } else{
-    const communityDoc = doc(db, "communities", id);
-    await deleteDoc(communityDoc)
-  }}
+    if (admin !== currentUser.email) {
+      deleteAlert();
+    } else {
+      const communityDoc = doc(db, "communities", id);
+      await deleteDoc(communityDoc);
+    }
+  };
 
-  const updateCommunity = async () => {
-
-  }
+  const updateCommunity = async () => {};
 
   async function deleteAlert() {
     const alert = document.createElement("ion-alert");
@@ -63,67 +64,59 @@ const CommunitiesList = () => {
     console.log("onDidDismiss resolved with role", role);
   }
 
- 
-
   return (
-        <div id="communityCards" className="communitiesPage">
-          {communityList.map((community) => {
-            return (
-              <ion-card key={community.id}>
-                <Link to={`/page/CommunityPage/${community.id}`}>
-                  <img
-                    key={community.id}
-                    src={community?.communityPicture}
-                    alt="ion"
-                  ></img>
-                </Link>
-                <ion-card-header>
-                  <ion-card-title>{community.communityName}</ion-card-title>
-                </ion-card-header>
-                <ion-card-content>
-                  {community.communityCategory}
-                </ion-card-content>
-                <ion-card-content>
-                  {community.communityDescription}
-                </ion-card-content>
-                <ion-footer>
-                  <ion-row>
-                    <ion-col center text-center>
-                      <button>
-                        <ion-icon name="thumbs-up"></ion-icon>
-                        <div>
-                          Community Members:{" "}
-                          {community?.communityMembers?.length}
-                        </div>
-                      </button>
-                      <button
-                        onClick={() => {
-                          deleteCommunity(community.id, community.admin);
-                        }}
-                      >
-                        {" "}
-                        Delete Community
-                      </button>
-                      <button
-                        onClick={() => {
-                          <Link
-                            to={`/page/CommunityPage/${community.id}`}
-                          ></Link>;
-                        }}
-                      >
-                        {" "}
-                        Update Community
-                      </button>
-                    </ion-col>
-                    <ion-col center text-center></ion-col>
-                  </ion-row>
-                </ion-footer>
-              </ion-card>
-            );
-          })}
-        </div>
+    <div id="communityCards" className="communitiesPage">
+      {communityList.map((community) => {
+        return (
+          <ion-card key={community.id}>
+            <Link to={`/page/CommunityPage/${community.id}`}>
+              <img
+                key={community.id}
+                src={community?.communityPicture}
+                alt="ion"
+              ></img>
+            </Link>
+            <ion-card-header>
+              <ion-card-title>{community.communityName}</ion-card-title>
+            </ion-card-header>
+            <ion-card-content>{community.communityCategory}</ion-card-content>
+            <ion-card-content>
+              {community.communityDescription}
+            </ion-card-content>
+            <ion-footer>
+              <ion-row>
+                <ion-col center text-center>
+                  <button>
+                    <ion-icon name="thumbs-up"></ion-icon>
+                    <div>
+                      Community Members: {community?.communityMembers?.length}
+                    </div>
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteCommunity(community.id, community.admin);
+                    }}
+                  >
+                    {" "}
+                    Delete Community
+                  </button>
+                  <button
+                    onClick={() => {
+                      <Link to={`/page/CommunityPage/${community.id}`}></Link>;
+                    }}
+                  >
+                    {" "}
+                    Update Community
+                  </button>
+                </ion-col>
+                <ion-col center text-center></ion-col>
+              </ion-row>
+            </ion-footer>
+          </ion-card>
+        );
+      })}
+    </div>
   );
-  
 };
 
 export default CommunitiesList;
--- src/pages/Communities/communityPage/communityPage.js
+++ src/pages/Communities/communityPage/communityPage.js
@@ -18,33 +18,66 @@ import { useCollection } from "react-query-firestore";
 import { useParams } from "react-router";
 
 const CommunityPage = (id) => {
-   const [postList, setPostsList] = useState([]);
-   const postsCollectionRef = collection(db, "posts");
-   let  communityId = useParams().id;
-  
+  const [postList, setPostsList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+  let communityId = useParams().id;
+
   // const community = communityId?.id;
 
- useEffect(() => {
-  const getCommunities = async () => {
-    const post = await getDocs(postsCollectionRef);
-    setPostsList(
-      post.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-    );
-  };
-  getCommunities();
- }, [])
+  useEffect(() => {
+    const queryPosts = async () => {
+      const postsQuery = query(
+        postsCollectionRef,
+        where("communityId", "==", communityId)
+      );
 
- console.log(useParams().id)
+      const postsQuerySnap = await getDocs(postsQuery);
+
+         setPostsList(
+           postsQuerySnap.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    };
+
+    queryPosts();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(useParams().id);
 
   return (
-        <div id="postCards" className="postPage">
-          {postList.map((post) => {
-            return (
-              <h1>{communityId}</h1>
+    <div id="communityCards" className="communitiesPage">
+      {postList.map((post) => {
+                    return (
+                      <ion-card key={post.id}>
+                        <img
+                          src={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fst.automobilemag.com%2Fuploads%2Fsites%2F11%2F2016%2F03%2F2015-Mitsubishi-Lancer-Evolution-Final-Edition-engine-1.jpg&f=1&nofb=1`}
+                          alt="ion"
+                        ></img>
 
-           );
-       })}
- </div>
+                        <ion-card-header>
+                          <ion-card-title>
+                            {post.postTitle}
+                          </ion-card-title>
+                        </ion-card-header>
+                        <ion-card-content>
+                          {post.postDescription}
+                        </ion-card-content>
+                        <ion-footer>
+                          <ion-row>
+                            <ion-col center text-center>
+                              <button>
+                                <ion-icon name="thumbs-up"></ion-icon>
+                              </button>
+                        
+                            </ion-col>
+                            <ion-col center text-center></ion-col>
+                          </ion-row>
+                        </ion-footer>
+                      </ion-card>
+                    );
+      })}
+    </div>
   );
   
 };
